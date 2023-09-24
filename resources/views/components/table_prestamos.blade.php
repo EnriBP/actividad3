090c3f9e080e8856a109bbd7369b788cd12d7da1
Enrique <114609169+EnriBP@users.noreply.github.com>
2023-09-25 00:03:36 +0200

    Add files via upload

--- resources/views/components/table_prestamos.blade.php
+++ resources/views/components/table_prestamos.blade.php
@@ -1,12 +1,29 @@
 <tr>
-    <td>{{ $prestamo->libro_id }}</td>
-    <td>{{ $prestamo->user_id }}</td>
-    <td>{{ $prestamo->fecha_prestamo }}</td>
-    <td>{{ $prestamo->fecha_devolucion }}</td>
-    <td>
-        <a href="http://127.0.0.1:8000/updatePrestamoForm/{{ $prestamo->id }}"><button>Cambiar Préstamo</button></a>
-        @if ($prestamo->fecha_devolucion=='null')
-            <a href="http://127.0.0.1:8000/updateDevolucionForm/{{ $prestamo->id }}"><button>Devolver</button></a>
+    @if ($users->rol == 'admin')
+        <td>{{ $prestamo->libro_id }}</td>
+        <td>{{ $prestamo->user_id }}</td>
+        <td>{{ $prestamo->fecha_prestamo }}</td>
+        <td>{{ $prestamo->fecha_devolucion }}</td>
+        <td>
+            <a href="http://127.0.0.1:8000/updatePrestamoForm/{{ $prestamo->id }}"><button>Cambiar Préstamo</button></a>
+            @if ($prestamo->fecha_devolucion=='null')
+                <a href="http://127.0.0.1:8000/updateDevolucionForm/{{ $prestamo->id }}"><button>Devolver</button></a>
+            @endif
+        </td>
+    @endif
+
+    @if ($users->rol == 'usuario')
+        @if ($users->email == $prestamo->user_id)
+            <td>{{ $prestamo->libro_id }}</td>
+            <td>{{ $prestamo->user_id }}</td>
+            <td>{{ $prestamo->fecha_prestamo }}</td>
+            <td>{{ $prestamo->fecha_devolucion }}</td>
+            <td>
+                <a href="http://127.0.0.1:8000/updatePrestamoForm/{{ $prestamo->id }}"><button>Cambiar Préstamo</button></a>
+                @if ($prestamo->fecha_devolucion=='null')
+                    <a href="http://127.0.0.1:8000/updateDevolucionForm/{{ $prestamo->id }}"><button>Devolver</button></a>
+                @endif
+            </td>
         @endif
-    </td>
+    @endif
 </tr>
