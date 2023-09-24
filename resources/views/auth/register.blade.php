0a1e52abbfe643a5f727cecdb73bb3546e510968
Enrique <114609169+EnriBP@users.noreply.github.com>
2023-09-25 00:01:59 +0200

    Add files via upload

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -29,6 +29,14 @@
                 <x-input id="password_confirmation" class="block mt-1 w-full" type="password" name="password_confirmation" required autocomplete="new-password" />
             </div>
 
+            <div class="mt-4">
+                <label for="rol">Rol</label>
+                <select id="rol" class="block mt-1 w-full" name="rol" required>
+                    <option value="admin">Admin</option>
+                    <option value="usuario">Usuario</option>
+                </select>
+            </div>
+
             @if (Laravel\Jetstream\Jetstream::hasTermsAndPrivacyPolicyFeature())
                 <div class="mt-4">
                     <x-label for="terms">
